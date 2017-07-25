910168e6ed97a87f15a487e2ecf1ba2714779e68
Kevin Bishop <k483913@CINMAC10969.local>
2017-07-24 23:22:11 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'setup_test',
+    version = '0.1',
+    description = 'used as a test to make packages easily installed via github',
+    url = 'http://github.com/kevinandrewbishop/setup_test',
+    author = 'Kevin Bishop',
+    author_email = 'kevinandrewbishop@gmail.com',
+    license = 'MIT',
+    packages = ['setup_test'],
+    zip_safe = False)
---
+++ setup_test/__init__.py
@@ -0,0 +1,3 @@
+msg = 'The package is installed successfully!'
+
+print(msg)
